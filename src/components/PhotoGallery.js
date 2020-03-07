e590442edb2ca62638f2fcb2e2a624b1de890376
Greg <g.rutherford10@gmail.com>
2020-03-07 15:49:54 +0000

    photoGallery now returns random photo array

--- src/components/PhotoGallery.js
+++ src/components/PhotoGallery.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
+import Unsplash, { toJson } from 'unsplash-js';
 import './PhotoGallery.css';
+
+const UNSPLASH_ACCESS_KEY =
+  '9MYmrZtlhv2mlunCa8XLS-obyMKzPWt8hUE6cF67QQU';
+const UNSPLASH_SECRET_KEY =
+  'u0eKTDSjqHDbQnmrJjcGwNlZvNS7f_79N6p2iztvKLc';
 
 class PhotoGallery extends Component {
   constructor(props) {
@@ -7,6 +13,20 @@ class PhotoGallery extends Component {
     this.state = {
       results: [],
     };
+  }
+
+  componentDidMount() {
+    const unsplash = new Unsplash({
+      accessKey: `${UNSPLASH_ACCESS_KEY}`,
+    });
+    unsplash.photos
+      .listPhotos(2, 15, 'latest')
+      .then(toJson)
+      .then(json => {
+        this.setState({
+          results: json,
+        });
+      });
   }
 
   render() {
